58e986c0527b97815d92cfcd80c0bcb447275983
zhangfuqiang <zhangfuqiang@vipkid.com.cn>
2018-01-22 17:20:00 +0800

    init

---
+++ src/main/java/com/snowwave/textsearch/TextSearchApplication.java
@@ -0,0 +1,12 @@
+package com.snowwave.textsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSearchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/snowwave/textsearch/TextSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snowwave.textsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
