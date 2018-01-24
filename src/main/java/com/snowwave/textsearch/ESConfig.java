25e61b57907f1c1d9d3d5f246f24f328edba002e
zhangfuqiang <zhangfuqiang@vipkid.com.cn>
2018-01-24 15:31:22 +0800

    add es to spring boot

---
+++ src/main/java/com/snowwave/textsearch/ESConfig.java
@@ -0,0 +1,38 @@
+package com.snowwave.textsearch;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+/**
+ * Created by zhangfuqiang on 2018/1/22.
+ */
+@Configuration
+public class ESConfig {
+
+    @Bean
+    public TransportClient client() throws UnknownHostException{
+
+        InetSocketTransportAddress node = new InetSocketTransportAddress(
+                InetAddress.getByName("localhost"),
+                9300
+        );
+        //这里可以new多个node 添加到client中
+
+        Settings settings = Settings.builder()
+                .put("cluster.name","elasticsearch")
+                .build();
+
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(node);
+        return client;
+    }
+
+}
--- src/main/java/com/snowwave/textsearch/TextSearchApplication.java
+++ src/main/java/com/snowwave/textsearch/TextSearchApplication.java
@@ -1,10 +1,81 @@
 package com.snowwave.textsearch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
 @SpringBootApplication
+@RestController
 public class TextSearchApplication {
+
+	@Autowired
+	private TransportClient transportClient;
+
+
+	@GetMapping("/index")
+	public String index(){
+		return "index";
+	}
+
+	@GetMapping("/get/book/novel")
+	@ResponseBody
+	public ResponseEntity get(@RequestParam(name = "id" , defaultValue = "") String id){
+
+		if (id.isEmpty()){
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		GetResponse response = this.transportClient.prepareGet("book","novel",id).get();
+
+		if (!response.isExists()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(response.getSource(), HttpStatus.OK);
+
+	}
+
+
+	@PostMapping("/add/book/novel")
+    public ResponseEntity add(@RequestParam(name = "title") String title,
+                              @RequestParam(name = "author") String author,
+                              @RequestParam(name = "word_count") int wordCount,
+                              @RequestParam(name = "publish_date")
+                                          @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                          Date publishDate){
+	    try {
+           XContentBuilder content =  XContentFactory.jsonBuilder()
+                    .startObject()
+                    .field("title",title)
+                    .field("author",author)
+                    .field("word_count",wordCount)
+                    .field("publish_date",publishDate.getTime())
+                    .endObject();
+            IndexResponse result = this.transportClient.prepareIndex("book","novel")
+                    .setSource(content)
+                    .get();
+            return new ResponseEntity(result.getId(),HttpStatus.OK);
+        } catch (IOException e){
+	        e.printStackTrace();
+	        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TextSearchApplication.class, args);
